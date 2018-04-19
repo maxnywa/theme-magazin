5d2a9ae02e7867a89c4d5c7d060eef1f44fb3069
MariiaAsieieva <maxnywa@mail.ru>
2018-04-19 13:51:44 +0300

    new task in gulpfile and plugins in index was added

--- app/js/init.js
+++ app/js/init.js
@@ -10,3 +10,16 @@ $('.trending-carousel').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
 });
+
+// Switch product items view
+$('.grid-thumbs').on('click', function(){
+    // Find cards and remove class row-card
+    $('.all-items-container .card').removeClass('row-card');
+    $('.all-items-container .col-4').removeClass('col-12');
+});
+
+$('.grid-list').on('click', function(){
+    // Find cards and add class row-card
+    $('.all-items-container .card').addClass('row-card');
+    $('.all-items-container .col-4').addClass('col-12');
+});
