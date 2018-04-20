7b638d5522f277ee7830df04e8bbd6436008b7f0
Larussa <larussabrajko@gmail.com>
2018-04-20 21:39:54 +0300

    categories_list price_Range page_04

--- app/js/init.js
+++ app/js/init.js
@@ -5,6 +5,24 @@ $('.grid-thumbs').on('click', function (){
     $('.all-items-container .col-4').removeClass('col-12');
 });
 
+
+// Slick carousel init
+$('.trending-carousel').slick({
+
+    arrows:false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+});
+
+// Switch product items view
+$('.grid-thumbs').on('click', function(){
+    // Find cards and remove class row-card
+    $('.all-items-container .card').removeClass('row-card');
+    $('.all-items-container .col-4').removeClass('col-12');
+});
+
 $('.grid-list').on('click', function () {
     //Find cards and add class row-card
     $('.medium-card').addClass('row-card');
