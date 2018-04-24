23dc811114741514b563183d821553b48f608586
Lera Kulikova <eklera0506@gmail.com>
2018-04-24 22:22:53 +0300

    created sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');//Init Gulp
 const autoprefixer = require('gulp-autoprefixer');//init autoprefixer
 const concat = require('gulp-concat');// Init Gulp concat
+const spritesmith = require('gulp.spritesmith'); // create sprite
+const merge = require('merge-stream'); // connection merge-stream
 
 //Autoprefixer
 gulp.task('autoprefixer',function () {
@@ -10,6 +12,7 @@ gulp.task('autoprefixer',function () {
         }))
         .pipe(gulp.dest('app/css'))
 });
+// Concat all css files
 gulp.task('concat-css',function () {
     gulp.src(['app/css/reset.css',
         'app/css/fonts.css',
@@ -19,7 +22,26 @@ gulp.task('concat-css',function () {
         .pipe(concat('style.css'))
         .pipe(gulp.dest('app/css'))
 });
+// Generate sprite
+gulp.task('sprite', function () {
+    let spriteData = gulp.src('source/sprite/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.css',
+            imgPath: '../img/sprite.png'
+        }));
 
+    let imgStream = spriteData.img
+        .pipe(gulp.dest('app/img/'));
+
+    let cssStream = spriteData.css
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('source/css/components/'));
+    return merge(imgStream, cssStream);
+});
 //Watch
 gulp.task('watch',['autoprefixer','concat-css'], function () {
     gulp.watch('source/css/**/*.css', ['autoprefixer']);
