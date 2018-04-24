a40c8045ecabc5b4598b1858d10d1c523f751674
MariiaAsieieva <maxnywa@mail.ru>
2018-04-24 16:12:57 +0300

    working on adaptive design of main page

--- app/js/init.js
+++ app/js/init.js
@@ -10,12 +10,34 @@ $('select').styler();
 
 // Slick carousel init
 $('.trending-carousel').slick({
-
     arrows:false,
     dots: true,
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive:[
+        {
+            breakpoint: 1199,
+            settings:{
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 991,
+            settings:{
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 767,
+            settings:{
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
 });
 
 // Switch product items view
